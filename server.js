a9655d56a0b43d6cec174fb0d4ecab6e50641543
Wenlong <wenlong@example.com>
2026-02-22 06:59:28 +0800

    添加 Secure cookie 设置

--- server.js
+++ server.js
@@ -246,6 +246,7 @@ app.post('/api/login', async (req, res) => {
   // 设置 Cookie
   res.cookie('token', token, {
     httpOnly: true,
+    secure: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     sameSite: 'lax'
   });
